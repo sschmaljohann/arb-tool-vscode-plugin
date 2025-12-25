9ea7d5de0d0aa565c2ebfcd8d72fb3bd41565440
Sören Schmaljohann <soeren.schmaljohann@gmail.com>
2025-12-25 23:16:38 +0100

    use normal string

--- src/extension.ts
+++ src/extension.ts
@@ -153,7 +153,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 
         if (!arbStringExtractionPrefix) {
-          arbStringExtractionPrefix = `l10n.`;
+          arbStringExtractionPrefix = 'l10n.';
         }
 
         const edit = new vscode.WorkspaceEdit();
